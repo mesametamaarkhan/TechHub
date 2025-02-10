5b289ca7a6300b0aafee1f6d6e93468f3d34f688
mesametamaarkhan <i221304@nu.edu.pk>
2025-02-10 21:03:30 +0500

    some more changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route, Router, useNavigate } from 'react-router-dom';
 import Home from './pages/Home.jsx';
-import Navbar from "./components/Navbar.jsx";
+// import Navbar from "./components/Navbar.jsx";
 import Footer from './components/Footer.jsx';
 import Shop from './pages/Shop.jsx';
 import ProductDetail from './pages/ProductDetail.jsx';
@@ -18,7 +18,7 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      <Navbar />
+      {/* <Navbar /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
